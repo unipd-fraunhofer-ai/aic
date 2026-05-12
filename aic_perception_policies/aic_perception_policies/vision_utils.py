c4961a8b53585037a0d6eee3b85510d07e670af7
Matteo Terreran <matteo.terreran@gmail.com>
2026-05-12 16:15:42 +0200

    Add simplified version of vision policy

---
+++ aic_perception_policies/aic_perception_policies/vision_utils.py
@@ -0,0 +1,70 @@
+"""
+A collection of utility functions for vision-based perception policies.
+"""
+import numpy as np
+from scipy.spatial.transform import Rotation
+
+from geometry_msgs.msg import Transform, TransformStamped
+from sensor_msgs.msg import Image, CameraInfo
+
+####################################################
+# Vision configuations
+
+# Mapping from object general class to object model ID (ply file name)
+CLASS_NAMES_MAP = {
+    "nic_card_mount": 4,
+    "task_board_base": 1,
+    "sc_port": 5,
+}
+
+# Predefined cable tip frames and their transforms relative to the gripper TCP
+CABLE_TIP_FRAMES = {
+    'sc_tip_link': {
+        't_gripper_to_tip': [-0.0005699, -0.0005699, 0.0096407],
+        'q_gripper_to_tip': [-0.2298133984379278, 0.22655156773159751, -0.6627472977643364, -0.6757412205316223],
+    },
+    'sfp_tip_link': {
+        't_gripper_to_tip': [-0.000, -0.020687, 0.054119],
+        'q_gripper_to_tip': [-0.17785966749625665, -0.00503708733179058, 0.027383843138112103, -0.983661891514159],
+    }
+}
+
+
+####################################################
+# Load functions
+def load_intrinsics(camera_info_msg: CameraInfo):
+    k = camera_info_msg.k
+    fx = float(k[0])
+    fy = float(k[4])
+    cx = float(k[2])
+    cy = float(k[5])
+    return {
+        "fx": fx,
+        "fy": fy,
+        "cx": cx,
+        "cy": cy,
+        "width": camera_info_msg.width,
+        "height": camera_info_msg.height,
+        "depth_scale": 1.0,
+    }
+
+####################################################
+# Pose estimation utilities
+def get_object_model_id(class_name: str):
+    object_model_name = '_'.join(class_name.split("_")[:-1]) if class_name[-1].isdigit() else class_name
+    object_model_id = CLASS_NAMES_MAP.get(object_model_name, None)
+    return object_model_id
+
+###############################################
+# Transform utilities
+def transform_to_matrix(transform: Transform | TransformStamped) -> np.ndarray:
+    if isinstance(transform, TransformStamped):
+        transform = transform.transform
+    """Convert a geometry_msgs Transform to a 4x4 homogeneous transformation matrix."""
+    translation = transform.translation
+    rotation = transform.rotation
+    T = np.eye(4)
+    T[0:3, 3] = [translation.x, translation.y, translation.z]
+    r = Rotation.from_quat([rotation.x, rotation.y, rotation.z, rotation.w])
+    T[0:3, 0:3] = r.as_matrix()
+    return T
